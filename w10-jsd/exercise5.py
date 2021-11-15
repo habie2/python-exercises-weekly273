eda4175892af782e8fd7dab876d8943025e551c9
habie2 <100472296@alumnos.uc3m.es>
2021-11-15 21:48:23 +0100

    w10-5terminado

--- w10-jsd/exercise5.py
+++ w10-jsd/exercise5.py
@@ -20,11 +20,13 @@ pop(list1): removes the last element of the list and returns its value.
 """
 general_list = [3, 45, 25, 12, 43, 21, 12, 12, 26, 4, 38, 6, 39, 33, 28]
 
-def verify(fun1, fun2):
+def verify(name_fun:str, fun1, fun2):
+    print(name_fun)
+    print(fun1)
     if fun1 == fun2:
-        return print('[OK]')
+        return print('[OK]\n')
     else:
-        return print('[FAIL]')
+        return print('[FAIL]\n')
 
 def own_count(lista: list, num: int):
     count = 0
@@ -34,9 +36,8 @@ def own_count(lista: list, num: int):
     return count
 
 def own_index(lista: list, num: int):
-    check = True
     count = 0
-    while check == True and count < len(lista):
+    while count < len(lista):
         if lista[count] == num:
             return count
         else:
@@ -48,32 +49,60 @@ def own_append(lista: list, num: int):
     return lista
  
 def own_insert(lista: list, num: int, num_index: int):
-    new_list = lista[:num_index] + [num] + lista[num_index:]
-    return new_list
+    lista = lista[:num_index] + [num] + lista[num_index:]
+    return lista
 
+def own_remove(lista: list, num: int):
+    count = 0
+    while count < len(lista):
+        if lista[count] == num:
+            lista = lista[:count] + lista[count + 1:]
+            return lista
+        else:
+            count += 1
+    return lista
 
-print('count:')
-print(own_count(general_list, 43))
-verify(own_count(general_list, 43), general_list.count(43))
+def own_removeAll(lista: list, num: int):
+    count = 0
+    while count < len(lista):
+        if lista[count] == num:
+            lista = lista[:count] + lista[count + 1:]
+            count -= 1
+        count += 1
+    return lista
 
-print('index:')
-print(own_index(general_list, 12))
-verify(own_index(general_list, 12), general_list.index(12))
+def own_clear(lista:list):
+    lista = None
+    return lista
 
-print('append:')
-lista_append1, lista_append2, lista_append3 = general_list, general_list, general_list
-'''Try lista_append1 = lista_append2 = lista_append3 = general_list'''
-print(own_append(lista_append1, 1))
-lista_append3.append(1)
-verify(own_append(lista_append2, 1),lista_append3 )
+def own_pop(lista: list):
+    value = lista[-1]
+    lista = lista[:-1]
+    return value
 
-print('insert:')
-lista_insert1, lista_insert2, lista_insert3 = general_list, general_list, general_list
-list_insert = own_insert(lista_insert1, 9, 7)
-print(list_insert)
-lista_insert3.insert(7, 9)
-verify(list_insert,lista_insert3)
-'''
-Hay quer resolver el problema de que las listas al declararlas con igual, 
-se van actualizando infinitamente en el tiempo :) 
-'''
+print(general_list, end='\n\n')
+
+verify('count:', own_count(general_list, 43), general_list.count(43))
+
+verify('index:', own_index(general_list, 12), general_list.index(12))
+
+lista_append1, lista_append2 = general_list[:],  general_list[:]
+lista_append2.append(1)
+verify('append:', own_append(lista_append1, 1),lista_append2 )
+
+lista_insert1, lista_insert2 = general_list[:],  general_list[:]
+lista_insert2.insert(7, 9)
+verify('insert:', own_insert(lista_insert1, 9, 7),lista_insert2)
+
+lista_remove1, lista_remove2 = general_list[:],  general_list[:]
+lista_remove2.remove(12)
+verify('remove:', own_remove(lista_remove1, 12),lista_remove2)
+
+lista_removeAll1  = general_list[:]
+verify('removeAll:', own_removeAll(lista_removeAll1, 12), [3, 45, 25, 43, 21, 26, 4, 38, 6, 39, 33, 28])
+
+lista_clear1, lista_clear2 = general_list[:],  general_list[:]
+verify('clear', own_clear(lista_clear1), lista_clear2.clear())
+
+lista_pop1, lista_pop2 = general_list[:],  general_list[:]
+verify('pop', own_pop(lista_pop1), lista_pop2.pop())
