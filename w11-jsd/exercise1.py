d6261629f31b311881cc9ee7c085ca8918d535da
habie2 <100472296@alumnos.uc3m.es>
2021-11-20 16:22:50 +0100

    na y menos

---
+++ w11-jsd/exercise1.py
@@ -0,0 +1,12 @@
+"""
+@title: exercise1
+@author: habi2
+@date: 11/11/2021
+
+Create the Student class, which represents a first year student. Its fields will be: name,
+surname, programming mark, algebra mark, calculus mark, physics mark,
+skills mark and humanities mark. Create an init method that receives values for all the
+fields and checks that the values for the marks are in the right range (if not, a zero will be
+assigned to the mark). Write a program that creates an object of this type, fills the fields asking
+the user by keyboard and prints them.
+"""
